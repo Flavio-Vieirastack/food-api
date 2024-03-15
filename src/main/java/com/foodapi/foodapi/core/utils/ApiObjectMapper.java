04ebc1813ead4724a5583e221dc2d22a4b99e2f1
Flavio Vieira <flavioemerson1@outlook.com>
2024-03-15 17:48:47 -0300

    melhorado controller de restaurante

--- src/main/java/com/foodapi/foodapi/core/utils/ApiObjectMapper.java
+++ src/main/java/com/foodapi/foodapi/core/utils/ApiObjectMapper.java
@@ -3,12 +3,15 @@ package com.foodapi.foodapi.core.utils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 @Component
 public class ApiObjectMapper<T> {
     @Autowired
     private ObjectMapper mapper;
 
     public T dtoToModel(Object dto, Class<T> model) {
+
         return  mapper.convertValue(dto, model);
     }
+
 }
