a4d248af5f0080d1b41f7938d5729c108137c9d3
Flavio Vieira <flavioemerson1@outlook.com>
2024-03-16 15:51:52 -0300

    adicionado repositorio para demais modelos

---
+++ src/main/java/com/foodapi/foodapi/repository/PaymentTypeRepository.java
@@ -0,0 +1,9 @@
+package com.foodapi.foodapi.repository;
+
+import com.foodapi.foodapi.model.PaymentType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentTypeRepository extends JpaRepository<PaymentType, Long> {
+}
---
+++ src/main/java/com/foodapi/foodapi/repository/PermissionRepository.java
@@ -0,0 +1,9 @@
+package com.foodapi.foodapi.repository;
+
+import com.foodapi.foodapi.model.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+}
