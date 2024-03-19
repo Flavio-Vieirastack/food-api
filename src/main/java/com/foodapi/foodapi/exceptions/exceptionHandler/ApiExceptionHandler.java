80f115f47de3aea1cfc559d565da39a228b87dc7
Flavio Vieira <flavioemerson1@outlook.com>
2024-03-19 14:44:56 -0300

    melhorado handler de erros globais

--- src/main/java/com/foodapi/foodapi/exceptions/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/foodapi/foodapi/exceptions/exceptionHandler/ApiExceptionHandler.java
@@ -5,30 +5,59 @@ import com.foodapi.foodapi.exceptions.EntityConflictException;
 import com.foodapi.foodapi.exceptions.EntityNotFoundException;
 import com.foodapi.foodapi.exceptions.exceptionBody.ExceptionBody;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+    // Essa extensão serve para capturar todos os erros lançados pelo spring
+    // E você pode dar override nesse metodo a baixo
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            @NotNull Exception ex, Object body, @NotNull HttpHeaders headers,
+            @NotNull HttpStatusCode statusCode, @NotNull WebRequest request
+    ) {
+        if (body == null) {
+            body = buildExceptionBody(ex.getMessage()).build();
+        } else if(body instanceof String) {
+            body = buildExceptionBody((String) body).build();
+        }
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> entityNotFound(@NotNull EntityNotFoundException ex) {
+    public ResponseEntity<?> entityNotFound(
+            @NotNull EntityNotFoundException ex, HttpHeaders headers,
+            HttpStatusCode statusCode, WebRequest request) {
         var exceptionBody = buildExceptionBody(ex.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionBody);
+        return handleExceptionInternal(ex, exceptionBody, headers, statusCode, request);
     }
+
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<?> badRequest(@NotNull BadRequestException ex) {
+    public ResponseEntity<?> badRequest(
+            @NotNull BadRequestException ex, HttpHeaders headers,
+            HttpStatusCode statusCode, WebRequest request) {
         var exceptionBody = buildExceptionBody(ex.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionBody);
+         return handleExceptionInternal(ex, exceptionBody, headers, statusCode, request);
     }
+
     @ExceptionHandler(EntityConflictException.class)
-    public ResponseEntity<?> badRequest(@NotNull EntityConflictException ex) {
+    public ResponseEntity<?> badRequest(
+            @NotNull EntityConflictException ex, HttpHeaders headers,
+            HttpStatusCode statusCode, WebRequest request) {
         var exceptionBody = buildExceptionBody(ex.getMessage()).build();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionBody);
+         return handleExceptionInternal(ex, exceptionBody, headers, statusCode, request);
     }
 
     private ExceptionBody.@NotNull ExceptionBodyBuilder buildExceptionBody(String message) {
@@ -36,4 +65,6 @@ public class ApiExceptionHandler {
         exceptionBody.date(LocalDateTime.now()).message(message);
         return exceptionBody;
     }
+
+
 }
