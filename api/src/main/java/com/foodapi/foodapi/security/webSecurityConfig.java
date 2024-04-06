4179d4ae5041857f96dee4dddfbf0ab51e426909
David Miller <davidmiller.dev0826@gmail.com>
2024-04-06 15:49:23 -0300

    modificado estrutura

--- api/src/main/java/com/foodapi/foodapi/security/webSecurityConfig.java
+++ api/src/main/java/com/foodapi/foodapi/security/webSecurityConfig.java
@@ -31,7 +31,7 @@ public class webSecurityConfig {
     }
 
     @Bean
-    private PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
